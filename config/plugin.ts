4e5457f673a949afd3eb620e346e932e653d830f
zenghaibi <289797421@qq.com>
2022-10-12 12:21:48 +0800

    使用egg-validate验证参数

--- config/plugin.ts
+++ config/plugin.ts
@@ -6,10 +6,15 @@ const plugin: EggPlugin = {
     enable: true,
     package: 'egg-view-nunjucks',
   },
-  // 启用自定义插件
+  // 启用egg-mongoose插件
   mongoose: {
     enable: true,
     package: 'egg-mongoose',
+  },
+  // 启动egg-validate 数据验证的插件
+  validate: {
+    enable: true,
+    package: 'egg-validate',
   },
 };
 
