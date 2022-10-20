b532fa0503eb10086033e57713b2af9a7f64e37e
zenghaibi <289797421@qq.com>
2022-10-20 14:23:51 +0800

    8-6 确定查询条件的类型

--- app/controller/work.ts
+++ app/controller/work.ts
@@ -3,6 +3,23 @@ import validateInput from '../decorator/inputValidate';
 const workCreateRules = {
   title: 'string',
 };
+/*
+### 查询列表的可变条件
+* 页数
+* 每页多少条记录返回哪些值 - projection / select
+* 关联哪些集合以及返回哪些值 - $lookup/populate
+* 排序 - sort = { key: value }
+* 查询条件 - query = { key: value }
+*/
+// 查询条件
+interface IndexCondition {
+  pageIndex?: number;
+  pageSize?: number;
+  select?: string | string[];
+  populate?: { path?: string; select?: string };
+  customSort?: Record<string, any>;
+  find?: Record<string, any>;
+}
 
 export default class WorkController extends Controller {
   @validateInput(workCreateRules, 'workValidateFail')
