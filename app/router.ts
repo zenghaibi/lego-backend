ac91e1441f0ea8337747933f33ae452f52b806a8
zenghaibi <289797421@qq.com>
2022-11-22 14:59:18 +0800

    添加路由前缀

--- app/router.ts
+++ app/router.ts
@@ -7,37 +7,38 @@ export default (app: Application) => {
   //   secret: app.config.jwt.secret,
   // });
   router.get('/', controller.home.index);
-  router.post('/api/users/create', controller.user.createByEmail);
+  router.prefix('/api');
+  router.post('/users/create', controller.user.createByEmail);
   // 这里使用egg-jwt 插件 (app.jwt 这里没有定义类文件所以直接断言成 any)
-  router.get('/api/users/getUserInfo', controller.user.show);
-  router.post('/api/users/loginByEmail', controller.user.loginByEmail);
+  router.get('/users/getUserInfo', controller.user.show);
+  router.post('/users/loginByEmail', controller.user.loginByEmail);
   // 生成手机验证码
-  router.post('/api/users/genVeriCode', controller.user.sendVeriCode);
+  router.post('/users/genVeriCode', controller.user.sendVeriCode);
   // 手机号+短信验证码登录
   router.post(
-    '/api/users/loginByPhoneNumber',
+    '/users/loginByPhoneNumber',
     controller.user.loginByCellphone,
   );
   // gitee Oauth2
-  router.get('/api/users/passport/gitee', controller.user.oauth);
+  router.get('/users/passport/gitee', controller.user.oauth);
   router.get(
-    '/api/users/passport/gitee/callback',
+    '/users/passport/gitee/callback',
     controller.user.oauthByGitee,
   );
 
-  router.post('/api/works', controller.work.createWork);
-  router.get('/api/works', controller.work.myList);
-  router.get('/api/works/:id', controller.work.myWork)
-  router.get('/api/templates', controller.work.templateList);
-  router.patch('/api/works/:id', controller.work.update);
-  router.delete('/api/works/:id', controller.work.delete);
-  router.post('/api/works/publish/:id', controller.work.publishWork);
-  router.post('/api/works/publish-template/:id', controller.work.publishTemplate);
+  router.post('/works', controller.work.createWork);
+  router.get('/works', controller.work.myList);
+  router.get('/works/:id', controller.work.myWork)
+  router.get('/templates', controller.work.templateList);
+  router.patch('/works/:id', controller.work.update);
+  router.delete('/works/:id', controller.work.delete);
+  router.post('/works/publish/:id', controller.work.publishWork);
+  router.post('/works/publish-template/:id', controller.work.publishTemplate);
 
-  router.post('/api/utils/upload', controller.utils.uploadMutipleFiles);
-  router.get('/api/pages/:idAndUuid', controller.utils.renderH5Page);
-  router.post('/api/channel', controller.work.createChannel);
-  router.get('/api/channel/getWorkChannels/:id', controller.work.getWorkChannel);
-  router.patch('/api/channel/updateName/:id', controller.work.updateChannelName);
-  router.delete('/api/channel/:id', controller.work.deleteChannel);
+  router.post('/utils/upload', controller.utils.uploadMutipleFiles);
+  router.get('/pages/:idAndUuid', controller.utils.renderH5Page);
+  router.post('/channel', controller.work.createChannel);
+  router.get('/channel/getWorkChannels/:id', controller.work.getWorkChannel);
+  router.patch('/channel/updateName/:id', controller.work.updateChannelName);
+  router.delete('/channel/:id', controller.work.deleteChannel);
 };
