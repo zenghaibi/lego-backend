2078b6620fd654fb75a6764beb8e8dee46802cdc
zenghaibi <289797421@qq.com>
2022-12-06 11:14:19 +0800

    完成自动部署以及回滚,更新到1.0.4

--- app/controller/home.ts
+++ app/controller/home.ts
@@ -14,7 +14,7 @@ export default class HomeController extends Controller {
         dbVersion: version,
         redisStatus: status,
         appVersion,
-        hello: '完成自动部署以及回滚的功能'
+        hello: '完成自动部署以及回滚的功能，更新到1.0.4'
       },
     });
   }
