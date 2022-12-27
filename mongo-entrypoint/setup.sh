e16415efa531bb916e801c186d59b1d01a1855de
zenghaibi <289797421@qq.com>
2022-12-27 14:53:19 +0800

    添加3笔测试数据

--- mongo-entrypoint/setup.sh
+++ mongo-entrypoint/setup.sh
@@ -40,6 +40,32 @@ db.works.insertMany([
     isTemplate: true,
     isPublic: true,
     createdAt: '2020-11-26T09:27:19.000Z',
+  },
+  {
+    id: 21,
+    uuid: 'bacc',
+    title: '樊登解读意志力-复制',
+    desc: '未命名作品',
+    contentId: '5fa128349fa7b1005c9f0541',
+    publishContentId: '5fa128749fa7b1005c9f0542',
+    author: '136****5632',
+    coverImg: 'http://static-dev.imooc-lego.com/upload-files/screenshot-036563.png',
+    isTemplate: true,
+    status: 2,
+    copiedCount: 79,
+    latestPublishAt: '2020-11-04T01:28:16.000Z',
+    isHot: true,
+    isNew: true,
+    orderIndex: 0,
+    isPublic: true,
+    createdAt: '2020-11-03T09:51:48.000Z',
+    updatedAt: '2022-09-22T10:28:42.000Z',
+    user: {
+      userName: '136****5632',
+      nickName: 'Viking3',
+      gender: 0,
+      picture: 'http://imooc-lego-resource-dev.oss-cn-hangzhou.aliyuncs.com/logo-915499.png'
+    }
   }
 ])
 EOF
