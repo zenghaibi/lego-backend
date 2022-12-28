67c60d74a538d63245f8f2f2646c80c671e9dee6
zenghaibi <289797421@qq.com>
2022-12-29 00:18:22 +0800

    重新初化管方提供数据

--- mongo-entrypoint/setup.sh
+++ mongo-entrypoint/setup.sh
@@ -18,48 +18,145 @@ db.createUser({
 db.createCollection('works')
 db.works.insertMany([
   {
+    id: 18,
+    title: '前端架构师直播海报',
+    desc: '未命名作品',
+    author: '136****5632',
+    coverImg: 'https://static.imooc-lego.com/upload-files/screenshot-889755.png',
+    copiedCount: 1766,
+    isHot: true,
+    createdAt: '2020-11-18T05:47:04.000Z',
+    user: {
+      username: '18080220865',
+      nickName: '乐高0865'
+    }
+  },
+  {
     id: 19,
     title: '1024 程序员日',
     desc: '1024 程序员日',
-    author: '18080220865',
+    author: '185****2625',
     coverImg: 'http://static-dev.imooc-lego.com/imooc-test/sZHlgv.png',
-    copiedCount: 737,
+    copiedCount: 1268,
     isHot: true,
-    isTemplate: true,
-    isPublic: true,
     createdAt: '2020-11-26T09:27:19.000Z',
+    user: {
+      username: '18080220865',
+      nickName: '乐高0865'
+    }
   },
   {
-    id: 20,
-    title: '测试第二数据',
-    desc: '1024 程序员日',
-    author: '18080220865',
-    coverImg: 'http://static-dev.imooc-lego.com/imooc-test/sZHlgv.png',
-    copiedCount: 737,
-    isHot: true,
-    isTemplate: true,
-    isPublic: true,
-    createdAt: '2020-11-26T09:27:19.000Z',
+  id: 20,
+    title: '招聘-慕课乐高',
+    desc: '招聘广告页',
+    author: '185****2625',
+    coverImg: 'https://static.imooc-lego.com/upload-files/screenshot-323204.png',
+    copiedCount: 628,
+    isHot: false,
+    createdAt: '2020-11-25T07:37:23.000Z',
+    user: {
+      username: '18080220865',
+      nickName: '乐高0865'
+    }
   },
   {
     id: 21,
-    uuid: 'bacc',
-    title: '樊登解读意志力-复制',
+    title: '未命名作品',
     desc: '未命名作品',
-    contentId: '5fa128349fa7b1005c9f0541',
-    publishContentId: '5fa128749fa7b1005c9f0542',
     author: '136****5632',
-    coverImg: 'http://static-dev.imooc-lego.com/upload-files/screenshot-036563.png',
-    isTemplate: true,
-    status: 2,
-    copiedCount: 79,
-    latestPublishAt: '2020-11-04T01:28:16.000Z',
-    isHot: true,
-    isNew: true,
-    orderIndex: 0,
-    isPublic: true,
-    createdAt: '2020-11-03T09:51:48.000Z',
-    updatedAt: '2022-09-22T10:28:42.000Z',
-  }
+    coverImg: 'https://static.imooc-lego.com/upload-files/screenshot-677311.png',
+    copiedCount: 340,
+    isHot: false,
+    createdAt: '2020-11-23T06:24:17.000Z',
+    user: {
+      username: '18080220865',
+      nickName: '乐高0865'
+    }
+  },
+  {
+    id: 23,
+    title: '慕课资讯',
+    desc: '程序员的早读时间',
+    author: '136****5632',
+    coverImg: 'https://static.imooc-lego.com/upload-files/screenshot-726751.png',
+    copiedCount: 276,
+    isHot: false,
+    createdAt: '2020-11-18T14:48:36.000Z',
+    user: {
+      username: '18080220865',
+      nickName: '乐高0865'
+    }
+  },
+  {
+    id: 24,
+    title: '每日分享',
+    desc: '每日分享卡片',
+    author: '136****5632',
+    coverImg: 'https://static.imooc-lego.com/upload-files/screenshot-682056.png',
+    copiedCount: 213,
+    isHot: false,
+    createdAt: '2020-11-18T14:41:17.000Z',
+    user: {
+      username: '18080220865',
+      nickName: '乐高0865'
+    }
+  },
+  {
+    id: 22,
+    title: '慕课 Live Java 入门编程课',
+    desc: '慕课 Live 只需3天 每天1.5 小时 0 基础新人 Java 入门编程课',
+    author: '136****5632',
+    coverImg: 'https://static.imooc-lego.com/upload-files/screenshot-133701.png',
+    copiedCount: 161,
+    isHot: false,
+    createdAt: '2020-11-21T09:28:24.000Z',
+    user: {
+      username: '18080220865',
+      nickName: '乐高0865'
+    }
+  },
+  {
+    id: 25,
+    title: '慕课 Live',
+    desc: '这是慕课 live 如何搭建一套混合移动应用架构的海报',
+    author: '136****5632',
+    coverImg: 'https://static.imooc-lego.com/upload-files/screenshot-649919.png',
+    copiedCount: 96,
+    isHot: false,
+    createdAt: '2020-11-18T12:36:49.000Z',
+    user: {
+      username: '18080220865',
+      nickName: '乐高0865'
+    }
+  },
+  {
+    id: 28,
+    title: '中秋快乐',
+    desc: '中秋快乐',
+    author: '159****5014',
+    coverImg: 'http://static.imooc-lego.com/upload-files/screenshot-388804.png',
+    copiedCount: 83,
+    isHot: false,
+    createdAt: '2020-11-18T09:15:48.000Z',
+    user: {
+      username: '18080220865',
+      nickName: '乐高0865'
+    }
+  },
+  {
+    id: 27,
+    title: '樊登读书-意志力',
+    desc: '樊登读书-意志力',
+    author: '159****5014',
+    coverImg: 'http://static.imooc-lego.com/upload-files/screenshot-126349.png',
+    copiedCount: 81,
+    isHot: false,
+    createdAt: '2020-11-18T11:16:11.000Z',
+    user: {
+      username: '18080220865',
+      nickName: '乐高0865'
+      }
+    }
+  } 
 ])
 EOF
