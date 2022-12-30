4fa47f1807ee1625834c48fb4452472f317b241c
zenghaibi <289797421@qq.com>
2022-12-30 19:12:19 +0800

    修改返回返回数的在类型error=> errno

--- app/extend/helper.ts
+++ app/extend/helper.ts
@@ -14,7 +14,7 @@ interface ErrorRespType {
 export default {
   success({ ctx, res, msg }: RespType) {
     ctx.body = {
-      error: 0,
+      errno: 0,
       data: res ? res : null,
       message: msg ? msg : '请求成功',
     };
