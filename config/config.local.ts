ec4183477702b349fec367669886ff0b625edd1b
曾海碧 <linuxangel@126.com>
2023-05-29 02:16:10 +0800

    开发环境也支持跨域

--- config/config.local.ts
+++ config/config.local.ts
@@ -3,5 +3,10 @@ import { EggAppConfig, PowerPartial } from 'egg';
 export default () => {
   const config: PowerPartial<EggAppConfig> = {};
   config.baseUrl = 'http://localhost:7001';
+
+  // 2 配置 cors 允许的域名
+  config.security = {
+    domainWhiteList: ['*']
+  }
   return config;
 };
